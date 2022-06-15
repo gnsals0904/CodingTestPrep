4e48726650e7252b70d4bdb01185e154db17fdd9
gnsals0904 <gnsals0904@naver.com>
2022-06-15 23:18:34 +0900

    problem solve

---
+++ Baekjoon/3003.py
@@ -0,0 +1,8 @@
+a, b, c, d, e, f = map(int, input().split())
+A = 1
+B = 1
+C = 2
+D = 2
+E = 2
+F = 8
+print(A - a, B - b, C - c, D - d, E - e, F - f)
---
+++ Baekjoon/5522.py
@@ -0,0 +1,5 @@
+result = 0
+for i in range(5):
+    temp = int(input())
+    result = result + temp
+print(result)
