a64e3665d07cae14b26110592baf0c69fdc40794
gnsals0904 <gnsals0904@naver.com>
2022-07-16 18:33:23 +0900

    2 pro solve

---
+++ Baekjoon/15921.py
@@ -0,0 +1,3 @@
+n = int(input())
+grade = list(map(int, input().split()))
+avg = sum(grade) / len(grade)
---
+++ Baekjoon/20492.py
@@ -0,0 +1,4 @@
+num = int(input())
+r1 = num // 100 * 78
+r2 = num - num // 100 * 20 // 100 * 22
+print(r1, r2)
