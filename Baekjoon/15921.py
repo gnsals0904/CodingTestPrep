5911c22df6cae54e61b19c5b64a31f18c261bf1d
gnsals0904 <gnsals0904@naver.com>
2022-07-17 18:00:00 +0900

    1 pro solves

--- Baekjoon/15921.py
+++ Baekjoon/15921.py
@@ -1,3 +1,12 @@
 n = int(input())
-grade = list(map(int, input().split()))
-avg = sum(grade) / len(grade)
+if n == 0:
+    print("divide by zero")
+else:
+    grade = list(map(int, input().split()))
+    avg = sum(grade) / len(grade)
+    temp_sum = 0
+    for i in range(len(grade)):
+        temp_sum = temp_sum + grade[i] / len(grade)
+    result = avg/temp_sum
+    str = "{:.2f}".format(result)
+    print(str)
