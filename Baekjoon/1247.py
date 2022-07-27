7f48733b9dd904b5f185ebf6a13f93e5e8d1b68b
gnsals0904 <gnsals0904@naver.com>
2022-07-27 18:43:29 +0900

    1 pro fix

--- Baekjoon/1247.py
+++ Baekjoon/1247.py
@@ -1,8 +1,10 @@
+import sys
+
 for i in range(3):
-    t = int(input())
+    t = int(sys.stdin.readline().strip())
     num_sum = 0
     for j in range(t):
-        n = int(input())
+        n = int(sys.stdin.readline().strip())
         num_sum = num_sum + n
     if num_sum > 0:
         print("+")
