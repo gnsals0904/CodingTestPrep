73a23fd72806632cb96edb7ab4b9dc4d32fa60e9
gnsals0904 <gnsals0904@naver.com>
2022-07-28 18:53:05 +0900

    1 pro solve but 2 pro try

--- Baekjoon/1598.py
+++ Baekjoon/1598.py
@@ -1,7 +1,15 @@
 n, m = map(int, input().split())
-n1 = n // 4 + 1
-n2 = 4 - n % 4
-m1 = m // 4 + 1
-m2 = 4 - m % 4
+if n % 4 == 0:
+    n1 = n // 4
+    n2 = 0
+else:
+    n1 = n // 4 + 1
+    n2 = 4 - n % 4
+if m % 4 == 0:
+    m1 = m // 4
+    m2 = 0
+else:
+    m1 = m // 4 + 1
+    m2 = 4 - m % 4
 result = max(n2, m2) - min(n2, m2) + max(n1, m1) - min(m1, n1)
 print(result)
