fadf83c82905cffdc8259886e06d04d0c2119a4a
gnsals0904 <gnsals0904@naver.com>
2022-07-29 22:28:32 +0900

    1 pro solve

--- Baekjoon/1284x.py
+++ Baekjoon/1284x.py
@@ -1,13 +1,22 @@
 while True:
-    n_list = [3, 3, 3]
-    N = int(input())
-    str_n = str(N)
-    if N == 0:
+    N = input()
+    result = 0
+    if N == "0":
         break
     else:
-        for i in n_list:
-            if i == '1':
-                i = 2
-            elif i == '0':
-                i = 4
-    print(4+sum(n_list))
+        for i in range(len(N)):
+            if N[i] == '1':
+                result = result + 2
+            elif N[i] == '0':
+                result = result + 4
+            else:
+                result = result + 3
+        if len(N) == 1:
+            result = result + 2
+        elif len(N) == 2:
+            result = result + 3
+        elif len(N) == 3:
+            result = result + 4
+        else:
+            result = result + 5
+    print(result)
