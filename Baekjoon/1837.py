c03296aeed134ea081741c354f929788eb894b0d
gnsals0904 <gnsals0904@naver.com>
2022-08-03 18:29:00 +0900

    try 1837 problem but fail

---
+++ Baekjoon/1837.py
@@ -0,0 +1,17 @@
+P, K = map(int, input().split())
+n = K
+check = False
+a = [False, False] + [True] * (n - 1)
+primes = []
+for i in range(2, n + 1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2 * i, n + 1, i):
+            a[j] = False
+for n1 in primes:
+    if P % n1 == 0:
+        print("BAD", n1)
+        check = True
+        break
+if not check:
+    print("GOOD")
