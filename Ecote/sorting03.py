79ad141791f5499ce891f992a37cef5d14802ddc
gnsals0904 <gnsals0904@naver.com>
2023-08-08 01:32:29 +0900

    sorting finish

---
+++ Ecote/sorting03.py
@@ -0,0 +1,16 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+for i in range(K):
+    if A[i] < B[i]:
+        A[i], B[i] = B[i], A[i]
+    else:
+        break
+
+result = 0
+
+print(sum(A))
