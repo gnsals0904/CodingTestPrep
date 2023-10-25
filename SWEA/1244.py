2cc69c52c0d30b610309c1b806c4d666b03462a1
gnsals0904 <gnsals0904@naver.com>
2023-10-26 03:59:09 +0900

    solving 1244

--- SWEA/1244.py
+++ SWEA/1244.py
@@ -1,34 +1,25 @@
 t = int(input())
 for test_case in range(t):
     a, b = map(int, input().split())
-    a_list = []
-    change_a_list = []
     a = str(a)
-    for i in range(len(a)):
-        a_list.append(a[i])
-        change_a_list.append(a[i])
-    len_a_list = len(a_list)
-    max_value = 0
-    result_list = []
-    while b != 0:
+    len_a = len(a)
+    now_try = set([a])
+    next_try = set()
+
+    while b:
         b -= 1
-        for i in range(len_a_list):
-            for j in range(len_a_list):
-                if i == j:
-                    continue
-                else:
-                    temp = change_a_list[i]
-                    change_a_list[i] = change_a_list[j]
-                    change_a_list[j] = temp
-                    result = ''
-                    for k in range(len_a_list):
-                        result += change_a_list[k]
-                    result = int(result)
-                    result_list.append(result)
-                    change_a_list = a_list.copy()
-        max_value = max(result_list)
-        max_value = str(max_value)
-        for i in range(len_a_list):
-            a_list[i] = max_value[i]
-            change_a_list[i] = max_value[i]
-    print("#" + str(test_case + 1), max_value)
+        while now_try:
+            now = now_try.pop()
+            now = list(now)
+            for i in range(len_a):
+                for j in range(len_a):
+                    if i == j:
+                        continue
+                    else:
+                        now[i], now[j] = now[j], now[i]
+                        next_try.add(''.join(now))
+                        now[i], now[j] = now[j], now[i]
+        now_try, next_try = next_try, now_try
+
+    now_try = list(now_try)
+    print("#" + str(test_case + 1), max(now_try))
